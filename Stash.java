ad9f5094e56815d0ae4e018baf3ff0a2e06d68bc
kavindyaS007 <kavindyasamarasinghe@gmail.com>
2020-09-25 05:33:57 +0530

    commit1

--- Stash.java
+++ Stash.java
@@ -1,3 +1,31 @@
 public class Stash{
+	public String getStashId(){
+		return stashId;
+	}
+	public String getStashName(){
+		return stashName;
+	}
+	public String getStashMsg(){
+		return stashMsg;
+	}
 	
+	public void setStashId(String stashId){
+		this.stashId = stashId;
+	}
+	public void setStashName(String stashName){
+		this.stashName = stashName;
+	}
+	public void setStashMsg(String stashMsg){
+		this.stashMsg = stashMsg;
+	}
+	
+	public void addStash(){
+		//addStash
+	}
+	public void removeStash(){
+		//removeStash
+	}
+	public void clearAllStash(){
+		//clearAllStash
+	}
 }
